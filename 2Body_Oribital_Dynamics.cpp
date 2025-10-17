fdc42f3b71db685e6086c772d2f5ba9ab2e7a42f
roshanv05 <contactroshanv@gmail.com>
2025-10-17 12:48:35 -0500

    Added OOP Classes

--- 2Body_Oribital_Dynamics.cpp
+++ 2Body_Oribital_Dynamics.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
+class System{
+  public:
+    int time;
+};
+
+class Satellite{
+  public:
+    array<int,2> pos;
+    array<int, 2> vel;
+    int ang_mom;
+};
+
+class Orbit{
+  public:
+    int a;
+    int e;
+    int ang_mom;
+};
+
+
 int main() {
-  cout << "Hello World!";
-  return 0;
+  Satellite cubeSat;
+  cubeSat.pos = {1, 2};
+  cout << "Position X is " << cubeSat.pos[0];
 }
